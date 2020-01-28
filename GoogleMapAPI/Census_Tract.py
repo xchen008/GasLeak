f707cd7f02c4323278ae44284ca91fc66e9eec15
Xueru Chen <xchen008@citymail.cuny.edu>
2020-01-28 11:04:52 -0500

    Updated map

--- GoogleMapAPI/Census_Tract.py
+++ GoogleMapAPI/Census_Tract.py
@@ -11,7 +11,7 @@ csvfile = "FDNY2018.csv"
 
 # FUNCTION: Get Census Tract from Longitude and Latitude coordintes using the Census Beru's API which returns a JSON file
 def getCensusTract(longitude, latitude, retryRun=0):  # returns an array [CensusTrack, CensusBlock, CountyName]
-    url = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates?y={0}&x={1}&benchmark=Public_AR_Current&vintage=Current_Current&format=json".format(
+    url = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x={0}&y={1}&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&format=json".format(
         longitude, latitude)
     if retryRun == 11:  # Failed to get json data 11 times with this longitude and latitude so need to skip this one
         print("failed 11 times")
@@ -21,7 +21,7 @@ def getCensusTract(longitude, latitude, retryRun=0):  # returns an array [Census
         dataJSON = response.json()
         data = dataJSON["result"]
         track = data["geographies"]["Census Tracts"][0]["BASENAME"]
-        block = data["geographies"]["2010 Census Blocks"][0]["BLOCK"]
+        block = data["geographies"]["Census Blocks"][0]["BLOCK"]
         county = data["geographies"]["Counties"][0]["NAME"]
         return [str(track), str(block), str(county)]
     except:
@@ -34,13 +34,12 @@ def getCensusTract(longitude, latitude, retryRun=0):  # returns an array [Census
 # a) Will modify GasHistory_ConEdison.csv to have the CensusTract, CensusBlock, and CountyName columns
 
 df = pd.read_csv(csvfile)  # read the csv file and store to df
+df['CensusTract2010'] = None
 
-for row in range(21000, len(df)):
+for row in range(0, len(df)):
     # b) using the lat and long coords of each entry to find the census data and adding to the respective arrays to add to csv col later
-    returnArray = getCensusTract(float(df.loc[row]["lat"].item()),
-                                 float(df.loc[row]["lon"].item()))  # data.iat[i,4] = lat   # data.iat[i,5] = lon
-    df.iat[row, 6] = returnArray[0]
-    df.iat[row, 7] = returnArray[1]
-    df.iat[row, 8] = returnArray[2]
+    returnArray = getCensusTract(float(df.loc[row]["lon"].item()),
+                                 float(df.loc[row]["lat"].item()))  # data.iat[i,4] = lat   # data.iat[i,5] = lon
+    df.iat[row, 9] = returnArray[0]
 
-df.to_csv("FDNY_21000.csv", encoding='utf-8',index=False)
+df.to_csv(csvfile, encoding='utf-8',index=False)
--- GoogleMapAPI/Graph.py
+++ GoogleMapAPI/Graph.py
@@ -5,28 +5,79 @@ import pandas as pd
 import geopandas as gpd
 import platform
 import matplotlib.pyplot as plt
+import contextily as ctx
 
-us_tract = gpd.read_file('tl_2019_36_tract/tl_2019_36_tract.shp')
+#shp_file = 'tl_2019_36_tract/tl_2019_36_tract.shp'
+shp_file = 'nyu_2451_34505/nyu_2451_34505.shp'
+#shp_file = 'NYC_Census_Tracts_for_2010_US_Census/NYC_Census_Tracts_for_2010_US_Census.shp'
+us_tract = gpd.read_file(shp_file)
 #us_tract.plot()
 #plt.show()
 #print(list(us_tract))
 data = pd.read_csv('CT_Monthly_Count2018.csv')
+#%%
 Month_data = pd.DataFrame(columns = ['Time','CensusTract','NumberOfReports']) # create a new Dataframe with each month
 for row in range(0,len(data)):
-    if data.iat[row,0][0:2] == '12':    
+    if data.iat[row,0][0:2] == '01':    
         Month_data = Month_data.append({'Time' : data.iat[row,0], 'CensusTract' : data.iat[row,1],  'NumberOfReports': data.iat[row,2] } , ignore_index=True)
 
 #%%
-ok_tract = us_tract[us_tract['STATEFP'] == '36']                        # select all data
-ok_tract['CensusTract'] = ok_tract['NAME'].str.upper()              # create a new column of the same name as DF from 'NAME'
+us_tract['CensusTract'] = us_tract['name'].str.upper()              # create a new column of the same name as DF from 'NAME'
                                                                     # In shp file, the "NAME" is the census tract
-ok_tract['CensusTract'] = ok_tract.CensusTract.astype(float)        # Conver type to float so we can merge the DFs
+us_tract['CensusTract'] = us_tract.CensusTract.astype(float)        # Conver type to float so we can merge the DFs
                                                                  # We use merge to pass in the data(NumOfReports)from the csv
-join = ok_tract.merge(Month_data, on='CensusTract')
+Month_data['NumberOfReports'] = Month_data.NumberOfReports.astype(int)
+#join = us_tract.merge(Month_data, on='CensusTract')
+
 #%%
-map = join.plot(column='NumberOfReports',cmap='Greens',figsize = (10,8),legend = True)
-map.set_title(label = 'Number of Reports per Census Tract (December)', fontdict={'fontsize': 16}, loc='center')
-leg = map.get_legend()
-leg.set_title('Number Of Reports')
-leg.set_bbox_to_anchor((1.1,0.5,0.1,0.5))                          # Adjusted numbers to find the best location and size of the legend
+us_tract['NumberOfReports'] = 0
+for row in range(0,len(us_tract)):
+    
+    ind = Month_data.loc[Month_data['CensusTract'] == us_tract['CensusTract'][row]].index
+    if len(ind) is not 0:
+        us_tract.at[row,'NumberOfReports'] = Month_data['NumberOfReports'][ind[0]]
+
+print(us_tract['NumberOfReports'].sum())
+
+
+
+#%%
+#map = us_tract.plot(column='NumberOfReports',cmap='Greens',figsize = (10,8),alpha = 0.8,legend = True)#edgecolor = 'k'
+#leg = map.get_legend()
+#leg.set_title('Number Of Gas Leak Reports')
+#leg.set_bbox_to_anchor((1.1,0.5,0.1,0.5))                          # Adjusted numbers to find the best location and size of the legend
+#%%
+#plot with background map
+df = us_tract.to_crs(epsg=3857) 
+ax = df.plot(column='NumberOfReports',cmap='Greens',figsize = (10,8),alpha = 0.8, legend = True)
+ax.set_title(label = 'Number of Gas Leak Reports per Census Tract (January)', fontdict={'fontsize': 16}, loc='center')
+ctx.add_basemap(ax,zoom = 12)
+
+
+# %%
+
+
+
+
+# %%
+#print(us_tract['COUNTYFP'])
+
+#nyc_county = ['005','047','061','081','085']
+#nyc_tract = us_tract[(us_tract['COUNTYFP'] == nyc_county[0]) | (us_tract['COUNTYFP'] == nyc_county[1]) | (us_tract['COUNTYFP'] == nyc_county[2]) | (us_tract['COUNTYFP'] == nyc_county[3]) |( us_tract['COUNTYFP'] == nyc_county[4])]
+
+#count = 0
+#for row in range(0,len(Month_data)):
+#    if Month_data.iat[row,1] in us_tract['CensusTract']:
+#        print(Month_data.iat[row,1])
+#        count += 1
+
+#print(count)
+
+
+
+# %%
+
+#print(nyc_tract.head)
+
+
 # %%
